eae7679ac0dc51f2c6fbf3706c8121ee5420b613
sudj <sudj@rose-hulman.edu>
2019-01-27 21:28:14 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,7 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Daniel Su and Joe Law.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
-
-####### Do NOT attempt this assignment before class! #######
-
